5f6c125de32b490b4a445ee6a33d3a4b3e08f8d0
Bryansoares59d <bryan.carvalho2@etec.sp.gov.br>
2025-09-24 16:52:58 -0300

    Jogo da Velha - React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './game';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Bravacoders</h1>
+      <Game />
+    </div>
+  );
+}
